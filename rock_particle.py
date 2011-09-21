28fea798818efba9919b97822a5b3f214b574d97
Tom Alexander <craftkiller@gmail.com>
2011-09-20 23:16:47 -0400

    tweaking the rocks

--- rock_particle.py
+++ rock_particle.py
@@ -7,4 +7,4 @@ class rock_particle(particle):
     """Particles from rock balls"""
 
     def __init__(self, game, x_position, y_position):
-        particle.__init__(self, game, game.rock_particle_image, 5, 10, 100, x_position, y_position, 3000, 5000)
+        particle.__init__(self, game, game.rock_particle_image, 30, 10, 100, x_position, y_position, 2000, 3000)
